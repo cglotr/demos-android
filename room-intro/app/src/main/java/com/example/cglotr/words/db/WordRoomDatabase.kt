86373780530a608f1a60ac92a20f1252109c5be2
cglotr <amir.ariffin.920404@gmail.com>
2018-07-28 19:24:47 +0800

    room database

---
+++ room-intro/app/src/main/java/com/example/cglotr/words/db/WordRoomDatabase.kt
@@ -0,0 +1,31 @@
+package com.example.cglotr.words.db
+
+import android.arch.persistence.room.Database
+import android.arch.persistence.room.Room
+import android.arch.persistence.room.RoomDatabase
+import android.content.Context
+import com.example.cglotr.words.db.entity.Word
+
+@Database(entities = [Word::class], version = 1)
+abstract class WordRoomDatabase : RoomDatabase() {
+
+    companion object {
+        private var sInstance: WordRoomDatabase? = null
+
+        fun getDatabase(context: Context): WordRoomDatabase {
+            if (sInstance == null) {
+                synchronized(WordRoomDatabase::class) {
+                    if (sInstance == null) {
+                        sInstance = Room.databaseBuilder(
+                                context.applicationContext,
+                                WordRoomDatabase::class.java,
+                                "word_database"
+                        ).build()
+                    }
+                }
+            }
+
+            return sInstance!!
+        }
+    }
+}
