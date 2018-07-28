67d0acbe80e9d6669140300c67046dd1040a71af
cglotr <amir.ariffin.920404@gmail.com>
2018-07-28 18:33:47 +0800

    word dao

---
+++ room-intro/app/src/main/java/com/example/cglotr/words/db/dao/WordDao.kt
@@ -0,0 +1,19 @@
+package com.example.cglotr.words.db.dao
+
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.Query
+import com.example.cglotr.words.db.entity.Word
+
+@Dao
+interface WordDao {
+
+    @Query("DELETE from word_table")
+    fun deleteAll(): Any
+
+    @Query("SELECT * from word_table ORDER BY word ASC")
+    fun getAllWords(): List<Word>
+
+    @Insert
+    fun insert(word: Word): Any
+}
