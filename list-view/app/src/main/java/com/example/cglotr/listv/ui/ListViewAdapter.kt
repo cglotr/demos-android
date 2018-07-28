a15bdb19dda67d9a8771a667e8c8448384586ebf
cglotr <amir.ariffin.920404@gmail.com>
2018-07-28 12:07:48 +0800

    inflate layout

--- list-view/app/src/main/java/com/example/cglotr/listv/ui/ListViewAdapter.kt
+++ list-view/app/src/main/java/com/example/cglotr/listv/ui/ListViewAdapter.kt
@@ -1,17 +1,31 @@
 package com.example.cglotr.listv.ui
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.TextView
+import com.example.cglotr.listv.R
 
 class ListViewAdapter(context: Context): BaseAdapter() {
 
     private val mContext: Context = context
+    private val mNames = arrayListOf(
+        "1","1"
+    )
+
+    init {
+        for (i in 0..10) {
+            val len = mNames.size
+            val a = mNames[len - 2].toInt()
+            val b = mNames[len - 1].toInt()
+            mNames.add((a + b).toString())
+        }
+    }
 
     override fun getCount(): Int {
-        return 100
+        return mNames.size
     }
 
     override fun getItem(position: Int): Any {
@@ -23,8 +37,15 @@ class ListViewAdapter(context: Context): BaseAdapter() {
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val textView = TextView(mContext)
-        textView.text = "$position"
-        return textView
+        val layoutInflater = LayoutInflater.from(mContext)
+        val rowView = layoutInflater.inflate(R.layout.row_view, parent, false)
+
+        val nameView = rowView.findViewById<TextView>(R.id.textView_name)
+        nameView.text = "Fibonacci: ${mNames[position]}"
+
+        val numberView = rowView.findViewById<TextView>(R.id.textView_number)
+        numberView.text = "Row number: $position"
+
+        return rowView
     }
 }
