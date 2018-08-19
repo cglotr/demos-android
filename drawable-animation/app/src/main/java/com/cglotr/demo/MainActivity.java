7582642b142fdb95b9e5f3884f9eeb5dfb17ee44
cglotr <amir.ariffin.920404@gmail.com>
2018-08-19 17:45:07 +0800

    wifi animation demo

--- drawable-animation/app/src/main/java/com/cglotr/demo/MainActivity.java
+++ drawable-animation/app/src/main/java/com/cglotr/demo/MainActivity.java
@@ -1,13 +1,29 @@
 package com.cglotr.demo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    AnimationDrawable wifiAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView = findViewById(R.id.image);
+        imageView.setBackgroundResource(R.drawable.animation);
+
+        wifiAnimation = (AnimationDrawable) imageView.getBackground();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        wifiAnimation.start();
     }
 }
